8d40065547f106a9fd46234247d017ff4362e41b
Cem Aksoylar <caksoylar@gmail.com>
2020-11-27 13:35:59 -0800

    Use defines for secrets properly

--- users/caksoylar/rules.mk
+++ users/caksoylar/rules.mk
@@ -1,7 +1,9 @@
-SRC += rotary_lock.c
-
 ifeq ($(strip $(USE_SECRETS)), yes)
     ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
-        SRC += secrets.c
+        SRC += rotary_lock.c secrets.c
+    else
+        OPT_DEFS += -DNO_SECRETS
     endif
+else
+    OPT_DEFS += -DNO_SECRETS
 endif
