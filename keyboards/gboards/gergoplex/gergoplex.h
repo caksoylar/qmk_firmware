2c4733ab116a2679ed5e35bd296c46d53d90b36c
Cem Aksoylar <caksoylar@gmail.com>
2020-11-28 14:28:21 -0800

    Fix layout macro typo

--- keyboards/gboards/gergoplex/gergoplex.h
+++ keyboards/gboards/gergoplex/gergoplex.h
@@ -24,7 +24,7 @@ extern i2c_status_t mcp23018_status;
 uint8_t init_mcp23018(void);
 
 // Create a keymap using only KC_ prefixed keys
-#define LAYOUT_split_3x6_3( \
+#define LAYOUT_split_3x5_3( \
     L00, L01, L02, L03, L04,        R00, R01, R02, R03, R04, \
     L10, L11, L12, L13, L14,        R10, R11, R12, R13, R14, \
     L20, L21, L22, L23, L24,        R20, R21, R22, R23, R24, \
