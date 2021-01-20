07e7f3818547c57775c17f4b0fd8841d928cc998
Cem Aksoylar <caksoylar@gmail.com>
2021-01-19 17:29:59 -0800

    Add RGBLIGHT_SLEEP for Iris

--- keyboards/keebio/iris/keymaps/caksoylar/config.h
+++ keyboards/keebio/iris/keymaps/caksoylar/config.h
@@ -26,8 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 16
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-
-#define TAPPING_FORCE_HOLD
+#define RGBLIGHT_SLEEP
 
 // might need to flip encoder direction
 // #define ENCODER_DIRECTION_FLIP
