8982ef7e13fa7306e215d586567e922aef03d8f2
Cem Aksoylar <caksoylar@gmail.com>
2021-02-23 22:17:21 -0800

    Clean up and disable unused features

--- users/caksoylar/rules.mk
+++ users/caksoylar/rules.mk
@@ -1,3 +1,8 @@
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE   = no
+MAGIC_ENABLE       = no
+COMMAND_ENABLE     = no
+
 ifeq ($(strip $(USE_SECRETS)), yes)
     ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
         SRC += rotary_lock.c secrets.c
